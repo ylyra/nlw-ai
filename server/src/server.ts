940c914b5bca66832d565fb928c2db0a0311c845
Yan Lyra <yanlyra3@gmail.com>
2023-09-12 22:11:57 -0300

    chore: added cors to backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,17 @@
+import { fastifyCors } from '@fastify/cors';
 import 'dotenv/config';
-
 import { fastify } from "fastify";
+
 import { createTranscriptionRoute } from "./routes/create-transcription";
 import { generateAiCompletionRoute } from './routes/generate-ai-completion';
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 
 const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*'
+})
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
